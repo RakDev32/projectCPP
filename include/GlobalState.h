c90edd22de085c9833b6003233c9f5315e5ca9c8
rakDev <67396410+RakDev32@users.noreply.github.com>
2026-01-18 20:26:31 +0200

    Ensure DroppedPellet builds

--- include/GlobalState.h
+++ include/GlobalState.h
@@ -13,7 +13,7 @@ private:
 
     // Δεδομένα για τη ροή και την κατάσταση [cite: 86]
     float m_canvas_width = 1000.0f;
-    float m_canvas_height = 500.0f;
+    float m_canvas_height = 600.0f;
     int m_score = 0;
     float m_worldW = 4000.0f;
     float m_worldH = 4000.0f;
@@ -25,11 +25,15 @@ private:
     float m_viewH = 600.0f;
 
     Hunter* m_player = nullptr;
+    bool m_gameOver = false;
+    int m_minNpcCount = 12;
 
     // Συλλογή STL για τη διαχείριση των οντοτήτων [cite: 81, 82]
     std::vector<class Organism*> m_entities;
 
     std::vector<class Node*> m_food; // STL συλλογή για τα "φαγητά"
+    std::vector<class DroppedPellet*> m_pellets;
+    float m_time = 0.0f;
     
 
 
@@ -40,6 +44,7 @@ public:
     // Υποχρεωτικές μέθοδοι [cite: 87]
     static void destroyInstance();
     void init();
+    void reset();
     void update(float dt);
     void draw();
 
