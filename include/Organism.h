adc27bb30ad758f01b1f02f4f59783d9172314b9
rakDev <67396410+RakDev32@users.noreply.github.com>
2026-01-18 19:04:21 +0200

    Make graph nodes core gameplay

--- include/Organism.h
+++ include/Organism.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <vector>
+#include <utility>
 #include "Node.h"
 
 class Organism {
 protected:
     std::vector<Node*> m_nodes;
+    std::vector<std::pair<float, float>> m_nodeOffsets;
+    std::vector<std::pair<int, int>> m_edges;
+    std::vector<std::pair<float, float>> m_nodeVelocities;
 
     float m_x = 0.0f;
     float m_y = 0.0f;
@@ -24,7 +28,7 @@ public:
     // position
     float getX() const { return m_x; }
     float getY() const { return m_y; }
-    void setPosition(float x, float y) { m_x = x; m_y = y; }
+    void setPosition(float x, float y);
 
     // velocity
     float getVx() const { return m_vx; }
@@ -32,15 +36,27 @@ public:
     void setVelocity(float vx, float vy) { m_vx = vx; m_vy = vy; }
     
     float getRadius() const;
+    float getMass() const;
+    size_t getNodeCount() const { return m_nodes.size(); }
     void setRadius(float r);
     void growByArea(float eatenRadius);
+    void growNodeByArea(size_t nodeIndex, float eatenRadius);
+    void addNodeNear(size_t baseIndex, float radius);
     // nodes
     void addNode(Node* n);
+    void addNode(Node* n, float offsetX, float offsetY);
+    void addEdge(int fromIndex, int toIndex);
+    void drawEdges(float camX, float camY) const;
 
     // collisions
     bool checkCollisionWithNode(const Node* target) const;
+    int findCollidingNode(const Node* target) const;
+    bool checkCollisionWithOrganism(const Organism& other, int* outMyIndex, int* outOtherIndex) const;
+    void updateInfection(float dt);
+    void applyGraphForces(float dt);
+    void infectNode(size_t nodeIndex);
 
     // polymorphic API
     virtual void update(float dt) = 0;
-    virtual void draw() const = 0;
+    virtual void draw(float camX, float camY) const = 0;
 };
